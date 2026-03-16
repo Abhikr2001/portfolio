254cbad2e7be261bcc594756db6a5abac0b8ef6a
Abhikr2001 <123152383+Abhikr2001@users.noreply.github.com>
2026-03-16 15:27:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://portfolio-rust-iota-62.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Main route
